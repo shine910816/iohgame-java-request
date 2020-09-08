547624949e8c743c8181441dc8c7882c1cacfdb2
ss <sss>
2020-09-08 15:02:46 +0800

    add DAO

---
+++ src/main/java/com/iohgame/service/nba/avod/NbaAvodDao.java
@@ -0,0 +1,17 @@
+package com.iohgame.service.nba.avod;
+
+import com.iohgame.framework.connect.mysql.parameters.Dao;
+import com.iohgame.framework.utility.parameters.property.Builder;
+
+public class NbaAvodDao implements Dao
+{
+    public static class ContainerBuilder implements Builder<NbaAvodDao>
+    {
+
+        @Override
+        public NbaAvodDao build()
+        {
+            return null;
+        }
+    }
+}
