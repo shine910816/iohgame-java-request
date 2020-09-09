7af7fd3c81210080a77a75430d8c015f4cf0027d
shine910816 <shine910816@163.com>
2020-09-09 10:28:53 +0800

    m

--- src/main/java/com/iohgame/service/nba/avod/NbaAvodConnect.java
+++ src/main/java/com/iohgame/service/nba/avod/NbaAvodConnect.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.iohgame.framework.connect.base.ConnectBase;
 
@@ -14,10 +15,9 @@ public class NbaAvodConnect extends ConnectBase
 {
     private List<String> m_list = new ArrayList<>();
 
-    public NbaAvodConnect set()
+    public NbaAvodConnect set(String[] fileContents)
     {
-        String fileContent = "";
-        for (String avodId : fileContent.split("\n"))
+        for (String avodId : fileContents)
         {
             m_list.add("https://qa-image.nba.rakuten.co.jp/media/ad/" + avodId + "_0.xml");
         }
@@ -30,11 +30,17 @@ public class NbaAvodConnect extends ConnectBase
         {
             for (String requestUrl : m_list)
             {
+                LOG.info("Analyzing URL: " + requestUrl);
+
                 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
                 DocumentBuilder db = dbf.newDocumentBuilder();
                 InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(requestUrl);
                 Document doc = db.parse(is);
-                // TODO XML analysis
+
+                LOG.info("DOM implementation: " + doc.getImplementation());
+
+                Element channel = (Element) doc.getElementsByTagName("channel").item(0);
+                channel.getElementsByTagName("title").item(0).getNodeValue();
             }
         }
         catch (Exception e)
